02603da6035de9480c55a02fb8f42aa55aafbfe6
Jaromil <jaromil@dyne.org>
2017-04-10 14:04:12 +0200

    added writedown as submodule for easy rendering to docx and pdf
    rendering depends from installed pandoc, supports TOC and bibtex
    workflow can proceed on decode-whitepaper.md without any change

---
+++ config.zsh
@@ -0,0 +1,27 @@
+
+# Added by Writedown on Mon Apr 10 12:10:58 CEST 2017
+WRITEDOWN_TITLE="DECODE Whitepaper v0.2"
+WRITEDOWN_AUTHOR="Denis Roio, James Barritt, Jaap-Henk Hoepman, Mark de Villiers, Tom Demeyer"
+WRITEDOWN_AFFILIATION="DECODEproject.EU Reserch Consortium"
+WRITEDOWN_DATE="Monday 10 April 2017"
+WRITEDOWN_TAGS="[blockchain, privacy, architecture, sharing economy, platform cooperativism, implementation]"
+
+# number for each section
+WRITEDOWN_NRSEC=no
+# table of contents
+WRITEDOWN_TOC=yes
+# bibliographic citation style (see writedown/citstyle)
+WRITEDOWN_CITSTYLE=harvard-kings-college-london
+# font size
+WRITEDOWN_FONTSIZE=14pt
+# bibtex file for bibliographic sources
+WRITEDOWN_BIB=views/references.bib
+
+# different formats as supported by pandoc.
+# to activate uncomment and fill, then use dash (-) as first argument
+# i.e: ./writedown/render -
+# WRITEDOWN_OUTPUT_FORMAT=epub
+# WRITEDOWN_OUTPUT_EXTENSION=epub
+
+# default pandoc base command
+# WRITEDOWN_PANDOC="pandoc --smart --standalone -f markdown
