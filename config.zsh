cc77dcdf9523037b41455000ae8b75ca29d5ee4a
Jim Barritt <jimb@thoughtworks.com>
2017-09-27 11:58:46 +0100

    Multiple Changes: - Factored out the version to a variable - Set version to DRAFT - Made the authors a yaml list so they format correctly - Commented out the data so it auto puts current date - Configured to write a logfile of the process

--- config.zsh
+++ config.zsh
@@ -1,9 +1,13 @@
+DEBUG=0
+LOG="./writedown.log"
+
+WRITEDOWN_VERSION="DRAFT"
 
 # Added by Writedown on Mon Apr 10 12:10:58 CEST 2017
-WRITEDOWN_TITLE="DECODE Whitepaper v0.2"
-WRITEDOWN_AUTHOR="Denis Roio, James Barritt, Jaap-Henk Hoepman, Mark de Villiers, George Danezis, Tom Demeyer, Shehar Bano, Oleguer Sagarra"
+WRITEDOWN_TITLE="DECODE Whitepaper v${WRITEDOWN_VERSION}"
+WRITEDOWN_AUTHOR="[Denis Roio, James Barritt, Jaap-Henk Hoepman, Mark de Villiers, George Danezis, Tom Demeyer, Shehar Bano, Oleguer Sagarra]"
 WRITEDOWN_AFFILIATION="DECODEproject.EU Research Consortium"
-WRITEDOWN_DATE="Monday 10 April 2017"
+#WRITEDOWN_DATE="Monday 10 April 2017"
 WRITEDOWN_TAGS="[blockchain, privacy, architecture, sharing economy, platform cooperativism, implementation]"
 
 # number for each section
@@ -20,8 +24,11 @@ WRITEDOWN_BIB=views/references.bib
 # different formats as supported by pandoc.
 # to activate uncomment and fill, then use dash (-) as first argument
 # i.e: ./writedown/render -
-# WRITEDOWN_OUTPUT_FORMAT=epub
+#WRITEDOWN_OUTPUT_FORMAT=pdf
 # WRITEDOWN_OUTPUT_EXTENSION=epub
 
 # default pandoc base command
-# WRITEDOWN_PANDOC="pandoc --smart --standalone -f markdown
+# WRITEDOWN_PANDOC="pandoc --template=simple-template.tex decode-whitepaper.md  --latex-engine=xelatex -o publish/decode-whitepaper-vDRAFT.pdf"
+
+# --variable mainfont=\"Helvetica Neue\" --variable sansfont=Arial --variable monofont=\"Courier New\" --variable fontsize=12pt --variable version=DRAFT-0.1
+# --variable geometry=a4paper
