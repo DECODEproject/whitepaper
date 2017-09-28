a209c86ec5918b0c919b4ec14f14754aa4512976
Jim Barritt <jimb@thoughtworks.com>
2017-09-28 09:19:50 +0100

    Fixed authors in config and added listings config to options

--- config.zsh
+++ config.zsh
@@ -5,7 +5,7 @@ WRITEDOWN_VERSION="DRAFT"
 
 # Added by Writedown on Mon Apr 10 12:10:58 CEST 2017
 WRITEDOWN_TITLE="DECODE Whitepaper v${WRITEDOWN_VERSION}"
-WRITEDOWN_AUTHOR="[Denis Roio, James Barritt, Jaap-Henk Hoepman, Mark de Villiers, George Danezis, Tom Demeyer, Shehar Bano, Oleguer Sagarra]"
+WRITEDOWN_AUTHOR="[Denis Rojo, James Barritt, Jaap-Henk Hoepman, Mark de Villiers, George Danezis, Tom Demeyer, Shehar Bano, Oleguer Sagarra]"
 WRITEDOWN_AFFILIATION="DECODEproject.EU Research Consortium"
 #WRITEDOWN_DATE="Monday 10 April 2017"
 WRITEDOWN_TAGS="[blockchain, privacy, architecture, sharing economy, platform cooperativism, implementation]"
@@ -14,6 +14,7 @@ WRITEDOWN_TAGS="[blockchain, privacy, architecture, sharing economy, platform co
 WRITEDOWN_NRSEC=no
 # table of contents
 WRITEDOWN_TOC=yes
+WRITEDOWN_TOC_DEPTH=3
 # bibliographic citation style (see writedown/citstyle)
 WRITEDOWN_CITSTYLE=harvard-kings-college-london
 # font size
@@ -28,6 +29,7 @@ WRITEDOWN_BIB=views/references.bib
 # WRITEDOWN_OUTPUT_EXTENSION=epub
 
 # default pandoc base command
+#WRITEDOWN_PANDOC="pandoc --smart --standalone -f $input_format "
 # WRITEDOWN_PANDOC="pandoc --template=simple-template.tex decode-whitepaper.md  --latex-engine=xelatex -o publish/decode-whitepaper-vDRAFT.pdf"
 
 # --variable mainfont=\"Helvetica Neue\" --variable sansfont=Arial --variable monofont=\"Courier New\" --variable fontsize=12pt --variable version=DRAFT-0.1
